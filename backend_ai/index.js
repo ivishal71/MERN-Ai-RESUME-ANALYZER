eac41047bfe5c33f0596c29bfe0ef4fc46d2100e
vishal shankhdhar <vishalshankhdhar72@gmail.com>
2026-06-07 23:58:42 +0530

    updated node version

--- backend_ai/index.js
+++ backend_ai/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const crypto = require("crypto");
+// const crypto = require("crypto");
 
 const app = express();
 
 const PORT = 5000;
+app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
 require("./conn");
-app.use(cors());
+// app.use(cors());
 // app.use(
 //   cors({
 //     credentials: true,
